bf21f12f7dc9517594e884656bbad65ea3f5fd71
lorphy <lorphy@gmail.com>
2026-01-16 09:37:31 +0800

    modify vite.config.js, suit for dev and prod

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        //target: 'https://memorial-backend-xl42.onrender.com',
-        target:'http://localhost:5000',
-        changeOrigin: true
-      },
-      '/uploads': {
-        //target: 'https://memorial-backend-xl42.onrender.com',
-        target:'http://localhost:5000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true
+        },
+        '/uploads': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true
+        }
       }
     }
   }
